24e8b46df381e57ff88f96e10c8ea4cdcb37a90e
GE <g.esu8@studenti.unica.it>
2023-10-11 16:51:37 +0200

    Finished Adder. Started recognizer up to lang2 completed

--- basics/recognizer/lib/recognizer.ml
+++ basics/recognizer/lib/recognizer.ml
@@ -1,14 +1,33 @@
-let lang1 _ = failwith ""
+let rec lang1 l = 
+match l with
+| a::[] when (a = '0' || a = '1') -> true
+| [] -> false;
+| '0'::l1 -> lang1 l1
+| '1'::l1 -> lang1 l1
+| _  -> false
 
-let lang2 _ = failwith ""
+let lang2 l =
+let rec check_Ones lo = match lo with
+| [] -> true
+| '1'::l1 -> check_Ones l1
+| _ -> false
+in
 
-let lang3 _ = failwith ""
+match l with
+|[] -> true
+|'0'::l1 -> check_Ones l1
+|'1'::l1 -> check_Ones l1
+|_ ->false
 
-let lang4 _ = failwith ""
 
-let lang5 _ = failwith ""
+let lang3 _ = false
+
+let lang4 _ = false
+
+let lang5 _ = false
     
 let recognizers = [lang1;lang2;lang3;lang4;lang5]
                   
 let belongsTo w = List.map (fun f -> f w) recognizers
-  
+ 
+
