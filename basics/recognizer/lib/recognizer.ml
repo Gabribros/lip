3dc9b4991dae7dd934ea97dcd07fc10e5146077c
Gabribros <gabriele.esu02@gmail.com>
2023-10-13 14:04:44 +0200

    Recognizer Exercise done

--- basics/recognizer/lib/recognizer.ml
+++ basics/recognizer/lib/recognizer.ml
@@ -20,11 +20,43 @@ match l with
 |_ ->false
 
 
-let lang3 _ = false
+let lang3 l =
+let rec check_else lo = match lo with
+| '0'::[] -> true
+| '1'::l1 -> check_else l1
+| '0'::l1 -> check_else l1
+| _ -> false
+in
 
-let lang4 _ = false
+match l with
+|[] -> true
+|'0'::l1 -> check_else l1
+| _ ->false
 
-let lang5 _ = false
+
+let lang4 l =
+let rec belongs4 lo c = match lo with
+| [] when (c = 2)-> true
+| '1'::_ when (c = 2)-> false
+| '1'::l1 -> belongs4 l1 (c+1)
+| '0'::l1 -> belongs4 l1 c
+| _ -> false
+in
+belongs4 l 0
+
+let lang5 l = 
+let rec belongs5 lo o z = match lo with
+| '0'::l1 when( (o = 0 || o = 2) && z<2) -> belongs5 l1 0 (z+1)
+| '1'::l1 when ( (z = 0 || z = 2) && o<2) -> belongs5 l1 (o+1) 0
+| '0'::l1 when( o = 0 && z=2) -> belongs5 l1 0 1
+| '1'::l1 when ( z = 0 && o=2) -> belongs5 l1 1 0
+| [] when ((z = 0 || z=2 ) && (o =0 || o = 2)) -> true
+|_ -> false
+in
+match l with
+|[] -> false
+| _ -> belongs5 l 0 0
+
     
 let recognizers = [lang1;lang2;lang3;lang4;lang5]
                   
